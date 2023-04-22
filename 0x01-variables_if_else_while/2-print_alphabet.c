49697dbe680cba57eae039d55f65070ea6557554
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-21 21:30:25 -0700

    alphabets in lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabbets in lower case
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char dee;
+
+	for (dee = 'a'; dee <= 'z'; dee++)
+	{
+		putchar(dee);
+	}
+	putchar('\n');
+	return (0);
+}
