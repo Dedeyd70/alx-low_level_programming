b4ef484987d7ca71812855e5f835c7def2424d16
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-22 21:46:09 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Printing different combinations
+ *
+ * Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int r, u;
+
+	for (r = 48; r <= 57; r++)
+	{
+		for (u = 49; u <= 57; u++)
+		{
+			if (r != u && u > r)
+			{
+			putchar(r);
+			putchar(u);
+			putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
