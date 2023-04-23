b608fbd57d71fe7652286bac678cc9dfd49cf2b5
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-23 06:53:28 -0700

    combinations of 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Combination of 3 digits
+ *
+ * Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int e, f, g;
+
+	for (e = 48; e < 58; e++)
+	{
+		for (f = 49; f < 58; f++)
+		{
+			for (g = 50; g < 58; g++)
+			{
+				if (g > f && g > e)
+				{
+
+				putchar(e);
+				putchar(f);
+				putchar(g);
+				{
+					if (e != 55 || f != 56)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
