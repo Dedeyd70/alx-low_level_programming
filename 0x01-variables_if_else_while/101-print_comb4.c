a223bb7d0ad4441232c11a97ac2f14f26e6029f1
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-25 04:05:57 -0700

    completed all task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int e, f, g;
 
-	for (e = 48; e < 58; e++)
+	for (e = 48; e <= 57; e++)
 	{
-		for (f = 49; f < 58; f++)
+		for (f = 49; f <= 57; f++)
 		{
-			for (g = 50; g < 58; g++)
+			for (g = 50; g <= 57; g++)
 			{
 				if (g > f && f > e)
 				{
 					putchar(e);
 					putchar(f);
 					putchar(g);
-					if (e != 55 || f != 56)
+					if ((e != 55) || (f != 56) || (g != 57))
 					{
 						putchar(',');
 						putchar(' ');
