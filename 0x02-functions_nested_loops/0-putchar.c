22c70a3b4fda8debe1c5bfc811cb43883c612706
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-25 10:03:10 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - printing _putchar
+ * main - Printing _putchar
  *
- * Return: always 0(Success)
+ * Return: always 0 (Success)
  *
  */
 int main(void)
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
