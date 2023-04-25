32411263f89b00f12edbd40aec6f50f5fd54f995
Dedeyd70 <dorothyokoto@gmail.com>
2023-04-25 11:04:11 -0700

    aplhabets in lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Printing alphabets in lower cases
+ */
+void print_alphabet(void)
+{
+	int u;
+
+	for (u = 9; u <= 122; u++)
+	{
+		_putchar(u);
+	}
+	_putchar('\n');
+}
