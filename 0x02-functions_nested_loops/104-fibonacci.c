6384e120c8d332c470cc0d42333a62d0fe0eac04
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-01 09:11:48 -0700

    fibonacci printing 98 numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the first 98 fibonacci numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int d = 1, f = 2, c, dee = 2;
+
+	printf("%d, %d, ", d, f);
+
+	while (dee < 98)
+	{
+		c = d + f;
+		printf("%d, ", c);
+		d = f;
+		f = c;
+		dee++;
+	}
+
+	c = d + f;
+	printf("%d\n", c);
+	return (0);
+}
