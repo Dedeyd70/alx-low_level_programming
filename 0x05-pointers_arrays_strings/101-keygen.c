743878c60b1f0e702fc23c1b90bed24cc585075a
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-02 12:38:07 -0700

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that generates random valid passwords
+ * Return: always 0(Success)
+ */
+int main(void)
+{
+	srand(time(NULL));
+
+	char password[7];
+	int d, sum = 0;
+
+	for (d = 0; d < 6; d++)
+	{
+		password[d] = rand() % 10 + '0';
+		sum += password[d];
+	}
+
+	password[6] = (sum % 10) + '0';
+
+	for (d = 0; d < 7; d++)
+	{
+		putchar(password[d]);
+	}
+
+	return (0);
+}
