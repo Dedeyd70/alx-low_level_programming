2d394c069859e6b133b27b925a5cba5ba862ce32
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-05 11:10:36 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,16 @@ char *leet(char *s)
 {
 	int d;
 	int j;
-	char a[] = "aAeEo0tTlL";
-	char b[] = "4433007711";
+	char s1[] = "aAeEo0tTlL";
+	char s2[] = "4433007711";
 
 	for (d = 0; s[d] != '\0'; d++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (s[d] == a[j])
+			if (s[d] == s1[j])
 			{
-				s[d] = b[j];
+				s[d] = s2[j];
 				break;
 			}
 		}
