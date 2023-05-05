4e71f90691dc8ef526787df2cd495cf6c709a66a
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-04 18:49:49 -0700

    capitalization

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rot13 - encoding strings with rot13
+ * @s: the string
+ * Return: the encoded string
+ */
+char *rot13(char *s)
+{
+	int d, f;
+	char a[]
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,36 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * *cap_string - Capitalizing all words of a string
- * @s: string to be used
+ * @caps: string to be used
  * Return: pointer to new string
  */
-char *cap_string(char *s)
+char *cap_string(char *caps)
 {
-	i
+	int p = 0;
+
+	while (caps[p])
+	{
+		while (!(caps[p] >= 'a' && caps[p] <= 'z'))
+			p++;
+
+		if (caps[p - 1] == 32 ||
+		    caps[p - 1] == 9 ||
+		    caps[p - 1] == 10 ||
+		    caps[p - 1] == 44 ||
+		    caps[p - 1] == 59 ||
+		    caps[p - 1] == 46 ||
+		    caps[p - 1] == 33 ||
+		    caps[p - 1] == 63 ||
+		    caps[p - 1] == 34 ||
+		    caps[p - 1] == 40 ||
+		    caps[p - 1] == 41 ||
+		    caps[p - 1] == 123 ||
+		    caps[p - 1] == 125 || /** assume the ascii values from lines 18 **/
+		    p == 0)
+			caps[p] -= 32;
+		p++;
+	}
+	return (caps);
 }
