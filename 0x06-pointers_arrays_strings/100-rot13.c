8d4650a2233ffcbd79c63de8bbe81043db66e666
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-04 19:21:35 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,11 +2,24 @@
 
 /**
  * rot13 - encoding strings with rot13
- * @s: the string
+ * @r: the string
  * Return: the encoded string
  */
-char *rot13(char *s)
+char *rot13(char *r)
 {
-	int d, f;
-	char a[]
+	char *start = r;
+
+	while (*r)
+	{
+		if ((*r >= 97 && *r <= 109) || (*r >= 65 && *r <= 77))
+		{
+			*r += 13;
+		}
+		else if ((*r >= 110 && *r <= 122) || (*r >= 78 && *r <= 90))
+		{
+			*r -= 13;
+		}
+		++r;
+	}
+	return (start);
 }
