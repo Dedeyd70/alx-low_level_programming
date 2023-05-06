fb126c5632802128df9f40fb25f6b6cc9f6f6d5f
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-06 09:13:26 -0700

    adding two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,50 +2,51 @@
 #include <stdio.h>
 
 /**
- * infinite_add - Adding two numbers
- * @r: is the input value
- * @n1: first num
- * @n2: second num
- * @size_r: is the size of the buffer
- * Return:if result is not stored in r, function must return 0
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int d1 = 0, d2 = 0, dee, jay, mar, ner, sum = 0;
+	int d1 = 0, d2 = 0, j, d, dee, jay, sam = 0;
 
 	while (*(n1 + d1) != '\0')
 		d1++;
 	while (*(n2 + d2) != '\0')
 		d2++;
 	if (d1 >= d2)
-		jay = d1;
+		d = d1;
 	else
-		jay = d2;
-	if (size_r <= jay + 1)
+		d = d2;
+	if (size_r <= d + 1)
 		return (0);
-	r[jay + 1] = '\0';
+	r[d + 1] = '\0';
 	d1--, d2--, size_r--;
-	mar = *(n1 + d1) - 48, ner = *(n2 + d2) - 48;
-	while (jay >= 0)
+	dee = *(n1 + d1) - 48, jay = *(n2 + d2) - 48;
+	while (d >= 0)
 	{
-		dee = mar + ner + sum;
-		if (dee >= 10)
-			sum = dee / 10;
+		j = dee + jay + sam;
+		if (j >= 10)
+			sam = j / 10;
 		else
-			sum = 0;
-		if (dee > 0)
-		*(r + jay) = (dee % 10) + 48;
+			sam = 0;
+		if (j > 0)
+		*(r + d) = (j % 10) + 48;
 		else
-			*(r + jay) = '0';
-		if (d1 >= 0)
-			d1--, mar = *(n1 + d1) - 48;
+			*(r + d) = '0';
+		if (d1 > 0)
+			d1--, dee = *(n1 + d1) - 48;
 		else
-			mar = 0;
+			dee = 0;
 		if (d2 > 0)
-			d2--, ner = *(n2 + d2) - 48;
+			d2--, jay = *(n2 + d2) - 48;
 		else
-			ner = 0;
-		jay--, size_r--;
+			jay = 0;
+		d--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
