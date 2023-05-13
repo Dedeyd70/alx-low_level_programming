0b230f8d1ad32e6669c0fe8c8e3b9be1966164b7
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-12 20:19:04 -0700

    adding numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Adiitions of positive numbers
@@ -9,6 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
+	int d, j;
+	int result = 0;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (d = 1; d < argc; d++)
+	{
+		for (j = 0; argv[d][j] != '\0'; j++)
+		{
+			if (argv[d][j] < '0' || argv[d][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[d]);
+	}
+	printf("%d\n", result);
 	return (0);
 }
