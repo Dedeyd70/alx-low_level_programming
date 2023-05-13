a14c5f2b1cd753d4c27609ffc8cf66aefaf22f61
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-12 22:17:49 -0700

    multiplying numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,21 +6,23 @@
  * main - Multiplication of two numbers
  * @argc: The argument count
  * @argv: The argument vector
- * Return: always 0(success)
+ * Return: 0 or 1 for Error
  */
 int main(int argc, char *argv[])
 {
-	int d, jay, r;
-	if (argc != 3)
+	int d = 0;
+	int j = 0;
+
+	if (argc == 3)
+	{
+		d = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", j * d);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
-	d = atoi(argv[1]);
-	jay = atoi(argv[2]);
-	r = d * jay;
-
-	printf("%i\n", r);
 	return (0);
 }
