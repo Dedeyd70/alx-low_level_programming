bd8c7446aac441cc0f492d2ac59f67ea3ba7047c
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-22 11:48:32 -0700

    printing the name of a program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Printing program name
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
