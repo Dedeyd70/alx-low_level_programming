edec9324f8084990fa4a0d2ad83c2b4b4ef34b0f
Dedeyd70 <dorothyokoto@gmail.com>
2023-05-24 07:14:01 -0700

    Printing opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int d = 0;
+	int num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	unsigned char *p = (unsigned char *)main;
+
+	for (; d < num; d++)
+	{
+		printf("%.2x", p[d]);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
