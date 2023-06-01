dd527c2318baf8eee54bc9d392095f557402a3ca
dedeyd70 <dorothyokoto@gmail.com>
2023-06-01 06:40:13 -0700

    you are beat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printMsg - printing a function before the main
+ *
+ * Return: nothing
+ */
+void printMsg(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
