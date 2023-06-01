d30852c0f679ef3468ce4e36d8be2a2637c0d95f
dedeyd70 <dorothyokoto@gmail.com>
2023-06-01 11:41:34 -0700

    printing elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t sum = 0;
 
-	while (h != NULL)
+	while (h!= NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%u] %s\n", h->len, "(nill)");
+			printf("[%d] (nil)\n", h->len);
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		sum++;
