e57a861d11d81e7e171c6dbff150a4f29b22ec9d
dedeyd70 <dorothyokoto@gmail.com>
2023-06-01 12:22:48 -0700

    Hobelton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+section .data
+format db "hello, Holberton", 0
+newline db 10, 0
+
+section .text
+extern printf
+
+global main
+
+main:
+sub rsp, 8
+mov rdi, format
+xor eax, eax
+call printf
+
+mov rdi, newline
+xor eax, eax
+call printf
+
+add rsp, 8
+xor eax, eax
+ret
