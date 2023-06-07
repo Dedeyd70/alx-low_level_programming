75ebaeb0fb7fc3b48304dbda7e2b61b1bdb990b8
dedeyd70 <dorothyokoto@gmail.com>
2023-06-06 18:07:18 -0700

    print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const listint_t *detect_loop(const listint_t *head);
+
+/**
+ * detect_loop - detects if a loop exits in a linked list
+ * @head: A pointer to the head of the linked list.
+ * Return: The node where the loop starts, or NULL if the loop is found
+ */
+const listint_t *detect_loop(const listint_t *head)
+{
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (slow);
+	}
+	return (NULL);
+}
+
+
 /**
  * print_listint_safe - prints a listint_t linked list
  * @head: A pointer to the head of the linked list
